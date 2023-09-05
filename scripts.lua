2c286d4a189417e80036da552047f0ffb9f8fe2c
Shubham <shubham.thorat@gameskraft.com>
2023-09-05 21:51:55 +0530

    rest

--- scripts.lua
+++ scripts.lua
@@ -1,10 +1,6 @@
-
 wrk.method = "POST"
-wrk.body   = {
-    "key": "rest_client",
-    "value": "rest_value"
-}
-wrk.headers["Content-Type"] = "application/json"
+wrk.body   = string.format("key=rest_client_key&value=rest_client_value")
+wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
 wrk.path = "/"
 
 done = function(summary, latency, requests)
@@ -17,7 +13,7 @@ done = function(summary, latency, requests)
    io.write(string.format("\t\"bytes_transfer_per_sec\": %0.2f,\n", (summary.bytes/summary.duration)*1e6))
 
    io.write("\t\"latency_distribution\": [\n")
-   for _, p in pairs({ 50, 90, 99 }) do
+   for _, p in pairs({ 50, 75, 90, 99, 99.9, 99.99, 99.999, 100 }) do
       io.write("\t\t{\n")
       n = latency:percentile(p)
       io.write(string.format("\t\t\t\"percentile\": %g,\n\t\t\t\"latency_in_microseconds\": %d\n", p, n))
